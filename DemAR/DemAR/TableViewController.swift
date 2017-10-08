f27872f2ecf9ec5dad8f0191e43080e93bd33bb0
e155707 <e155707@ie.u-ryukyu.ac.jp>
2017-10-08 23:36:41 +0900

    change Main.Storyboard

---
+++ DemAR/DemAR/TableViewController.swift
@@ -0,0 +1,12 @@
+import UIKit
+import SceneKit
+import ARKit
+
+class TableViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //背景色を透明にする
+        self.view.backgroundColor = UIColor.clear
+    }
+}
+
